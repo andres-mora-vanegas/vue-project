75372170c07d585397eef3c5624eb15bc799898b
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2018-11-13 16:59:47 -0500

    [update] add materilializecss

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    .content
+        .modal
+            .modal-title
+            .modal-content
+        .modal-overlay
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus" scoped>
+.overlay
+    position fixed
+    background  black 
+    opacity 0.7
+    width 100%
+    height 100%
+    top 0px
+    left 0px
+.modal
+    posi
+</style>
+
+
